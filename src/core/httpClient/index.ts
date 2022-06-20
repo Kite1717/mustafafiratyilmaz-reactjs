dbb1562882c18bd85c03a4bb49b87d8351e43833
Mustafa Yilmaz <mustafa.yilmaz@kobil.com>
2022-06-20 22:52:20 +0300

    Add : config and httpclient services

---
+++ src/core/httpClient/index.ts
@@ -0,0 +1,67 @@
+/* eslint-disable camelcase */
+import { AxiosResponse } from "axios";
+import { apiBaseUrl } from "../config";
+
+interface Response<T> {
+  data: T;
+  status_code: number;
+  message: string;
+  user_id: string;
+}
+
+const axios = require("axios").default;
+
+type Methods = "POST" | "GET" | "DELETE" | "PUT" | "PATCH";
+
+axios.interceptors.response.use(
+  (response: any) => response,
+  (error: any) => {
+    if (!error.response) {
+      window.location.reload();
+    }
+
+    if (error?.response?.status === 401) {
+      window.open(`${window.location.origin}/logout`, "_self");
+      /* window.location.href = '/logout' */
+    } else {
+      throw error;
+    }
+  }
+);
+
+interface RequestSettings<R> {
+  path: string;
+  method: Methods;
+  body?: R;
+  params?: object;
+  responseType?: string;
+}
+
+class HttpClientService {
+  private API_URL: string = apiBaseUrl;
+
+  fetch<R, T>(config: RequestSettings<R>): Promise<Response<T>> {
+    return new Promise((resolve, reject) => {
+      axios({
+        method: config.method,
+        url: this.API_URL + config.path,
+        data: config.body,
+        params: config.params,
+        responseType: config.responseType,
+      })
+        .then((response: AxiosResponse) => {
+          resolve(response.data as Response<T>);
+        })
+        .catch((e: any) => {
+          try {
+            // eslint-disable-next-line prefer-promise-reject-errors
+            reject({ ...e.response, data: JSON.parse(e.response.data) });
+          } catch (error) {
+            reject(e.response);
+          }
+        });
+    });
+  }
+}
+
+export default new HttpClientService();
